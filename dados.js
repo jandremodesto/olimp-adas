35ceb04cce24d2a82fabb9016d3b9bbedd0becab
Jandre Modesto da Silva Filho <44142858+jandremodesto@users.noreply.github.com>
2024-09-11 17:31:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+/* Criando objetos dos atletas */
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos [idade], ela já é considerada uma das maiores promessas do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca por sua técnica e força. Com apenas 16 anos, ela já coleciona diversas medalhas em competições nacionais e internacionais.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô  judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com sua performance nas Olimpíadas de Tóquio. Aos [idade] anos, ela é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
+
+ 
